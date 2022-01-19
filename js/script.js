ec7d771f5f1c0a373efe624ea0017b6738a33070
avik-contrivance <97288726+avik-contrivance@users.noreply.github.com>
2022-01-19 14:37:57 +0530

    New changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-$(".menuBtn").click(function(){
-    $(".mobile-menu").show();
-    $(".menuBtn").hide();
-    $(".closeBtn").css('display','flex');
-    $(".closeBtn").css('background','#298D7F');
+$('#news-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    navText: ["<img src='./images/arrow-left.png'>","<img src='./images/arrow-right.png'>"],
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
 });
-$(".closeBtn").click(function(){
-    $(".mobile-menu").hide();
-    $(".menuBtn").show();
-    $(".closeBtn").hide();
-});
+
+new WOW().init();
